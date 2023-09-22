5bb5a5f6f3be00dae156220a35293581e94d8178
motorrr4ik <nikita29042001@yandex.ru>
2023-09-22 14:51:56 +0300

    comments added

--- lib/Utils/Utils.hpp
+++ lib/Utils/Utils.hpp
@@ -1,6 +1,7 @@
 #ifndef UTILS
 #define UTILS
 
+//--------utility class for constants and utilty functions--------
 #include <Arduino.h>
 #define DIODE_ADC_SIGNAL A7
 #define GENERATOR_SIGNAL_INPUT A6
@@ -9,6 +10,7 @@
 #define CONTROLLER_INPUT_OPERATING_VOLTAGE 5
 #define MAX_ADC_VALUE 1023
 #define ADC_GENERATOR_DELTA_STEP 3
+#define SERIAL_PORT_SPEED 115200
 
 namespace Utils{
     double fromADCToVoltage(int&);
